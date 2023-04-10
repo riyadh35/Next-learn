100130ae77ded402478b3056ba0cd13531fa4648
riyadmahmudkhan <mdriyadhossain356@gmail.com>
2023-04-10 23:38:56 +0600

    first commit

---
+++ src/pages/blog/[blogId].js
@@ -0,0 +1,11 @@
+
+
+function BlogDetails() {
+    return (
+        <div>
+            <h1>This is blog details page </h1>
+        </div>
+    )
+}
+
+export default BlogDetails
---
+++ src/pages/blog/index.js
@@ -0,0 +1,14 @@
+
+function index() {
+    return (
+        <div>
+            <h1> This is Blog list page</h1>
+            <p>Blog 1</p>
+            <p>Blog 2</p>
+            <p>Blog 3</p>
+            <p>Blog 4</p>
+        </div>
+    )
+}
+
+export default index
